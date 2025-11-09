3a56c154a598c45cf25100e8568d1aa2fac0bc3d
GRACENOBLE <gracenoble72@gmail.com>
2025-11-10 00:01:24 +0300

    feat(models): add Order, Product, and User structs for database schema

---
+++ internal/database/models/product.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"github.com/google/uuid"
+)
+
+type Product struct {
+	ID          uuid.UUID `gorm:"type:uuid;primaryKey;default:gen_random_uuid()" json:"id"`
+	Name        string    `gorm:"not null" json:"name"`
+	Description string    `gorm:"not null" json:"description"`
+	Price       float64   `gorm:"not null" json:"price"`
+	Stock       int64     `gorm:"not null" json:"stock"`
+	Category    string    `gorm:"not null" json:"category"`
+	UserID      uuid.UUID `gorm:"type:uuid;not null" json:"user_id"`
+}
---
+++ internal/database/models/user.go
@@ -0,0 +1,10 @@
+package models
+
+import "github.com/google/uuid"
+
+type User struct {
+	ID       uuid.UUID `gorm:"type:uuid;primaryKey;default:gen_random_uuid()" json:"id"`
+	Username string    `gorm:"not null" json:"username"`
+	Email    string    `gorm:"uniqueIndex;not null" json:"email"`
+	Password string    `gorm:"not null" json:"-"`
+}
